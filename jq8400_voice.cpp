2284e8b473ddaf007f12a682607c81b0065f6c7d
Felícito Escobar <felicito.escobar@outlook.com>
2020-09-21 23:37:13 -0600

    Actualizacion de comandos
    Se agrega la definicion de los comandos para reproducir los archivos del 01 al 30. También los seis niveles de volumen.

---
+++ jq8400_voice.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file jq8400_voice.cpp
+ * @author Felícito Manzano (felicito.manzano@detektor.com.sv)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+extern const char PLAY_FILE01[6];
+extern const char PLAY_FILE02[6];
+extern const char PLAY_FILE03[6];
+extern const char PLAY_FILE04[6];
+extern const char PLAY_FILE05[6];
+extern const char PLAY_FILE06[6];
+extern const char PLAY_FILE07[6];
+extern const char PLAY_FILE08[6];
+extern const char PLAY_FILE09[6];
+extern const char PLAY_FILE10[6];
+extern const char PLAY_FILE11[6];
+extern const char PLAY_FILE12[6];
+extern const char PLAY_FILE13[6];
+extern const char PLAY_FILE14[6];
+extern const char PLAY_FILE15[6];
+extern const char PLAY_FILE16[6];
+extern const char PLAY_FILE17[6];
+extern const char PLAY_FILE18[6];
+extern const char PLAY_FILE19[6];
+extern const char PLAY_FILE20[6];
+extern const char PLAY_FILE21[6];
+extern const char PLAY_FILE22[6];
+extern const char PLAY_FILE23[6];
+extern const char PLAY_FILE24[6];
+extern const char PLAY_FILE25[6];
+extern const char PLAY_FILE26[6];
+extern const char PLAY_FILE27[6];
+extern const char PLAY_FILE28[6];
+extern const char PLAY_FILE29[6];
+extern const char PLAY_FILE30[6];
+
+extern const char SET_VOLUME_VERY_LOW[5];
+extern const char SET_VOLUME_LOW[5];
+extern const char SET_VOLUME_MEDIUM[5];
+extern const char SET_VOLUME_HIGH[5];
+extern const char SET_VOLUME_VERY_HIGH[5];
+extern const char SET_VOLUME_MAXIMUM[5];
